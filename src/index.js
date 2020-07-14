5bac3f32cacd3bd85e55b66f68ac6db621c178f0
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-07-13 19:14:32 -0700

    Adds basic structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const canvas = document.getElementsByTagName("game-canvas");
+    const ctx = canvas.getContext('2d');
+
+    
+});
