fe7bdfa6737233389abc14d18cfbfefbf483d283
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-08-26 16:14:33 -0700

    Preloading all sprites

--- src/menu.js
+++ src/menu.js
@@ -52,6 +52,84 @@ ZOMBIE_DIE_FRAME_4.src = "./assets/zombie/death/Death_004.png";
 const ZOMBIE_DIE_FRAME_5 = new Image();
 ZOMBIE_DIE_FRAME_5.src = "./assets/zombie/death/Death_005.png";
 
+const HERO_IDLE_FRAME_0 = new Image();
+HERO_IDLE_FRAME_0.src = "./assets/soldier/idle/Idle_gun_000.png";
+
+const HERO_IDLE_FRAME_1 = new Image();
+HERO_IDLE_FRAME_1.src = "./assets/soldier/idle/Idle_gun_001.png";
+
+const HERO_IDLE_FRAME_2 = new Image();
+HERO_IDLE_FRAME_2.src = "./assets/soldier/idle/Idle_gun_002.png";
+
+const HERO_IDLE_FRAME_3 = new Image();
+HERO_IDLE_FRAME_3.src = "./assets/soldier/idle/Idle_gun_003.png";
+
+const HERO_IDLE_FRAME_4 = new Image();
+HERO_IDLE_FRAME_4.src = "./assets/soldier/idle/Idle_gun_004.png";
+
+const HERO_IDLE_FRAME_5 = new Image();
+HERO_IDLE_FRAME_5.src = "./assets/soldier/idle/Idle_gun_005.png";
+
+const HERO_IDLE_FRAME_6 = new Image();
+HERO_IDLE_FRAME_6.src = "./assets/soldier/idle/Idle_gun_006.png";
+
+const HERO_IDLE_FRAME_7 = new Image();
+HERO_IDLE_FRAME_7.src = "./assets/soldier/idle/Idle_gun_007.png";
+
+const HERO_WALK_FRAME_0 = new Image();
+HERO_WALK_FRAME_0.src = "./assets/soldier/walk/Walk_gun_000.png";
+
+const HERO_WALK_FRAME_1 = new Image();
+HERO_WALK_FRAME_1.src = "./assets/soldier/walk/Walk_gun_001.png";
+
+const HERO_WALK_FRAME_2 = new Image();
+HERO_WALK_FRAME_2.src = "./assets/soldier/walk/Walk_gun_002.png";
+
+const HERO_WALK_FRAME_3 = new Image();
+HERO_WALK_FRAME_3.src = "./assets/soldier/walk/Walk_gun_003.png";
+
+const HERO_WALK_FRAME_4 = new Image();
+HERO_WALK_FRAME_4.src = "./assets/soldier/walk/Walk_gun_004.png";
+
+const HERO_WALK_FRAME_5 = new Image();
+HERO_WALK_FRAME_5.src = "./assets/soldier/walk/Walk_gun_005.png";
+
+const HERO_SHOOT_FRAME_0 = new Image();
+HERO_SHOOT_FRAME_0.src = "./assets/soldier/shoot/Gun_Shot_000.png";
+
+const HERO_SHOOT_FRAME_1 = new Image();
+HERO_SHOOT_FRAME_1.src = "./assets/soldier/shoot/Gun_Shot_001.png";
+
+const HERO_SHOOT_FRAME_2 = new Image();
+HERO_SHOOT_FRAME_2.src = "./assets/soldier/shoot/Gun_Shot_002.png";
+
+const HERO_SHOOT_FRAME_3 = new Image();
+HERO_SHOOT_FRAME_3.src = "./assets/soldier/shoot/Gun_Shot_003.png";
+
+const HERO_SHOOT_FRAME_4 = new Image();
+HERO_SHOOT_FRAME_4.src = "./assets/soldier/shoot/Gun_Shot_004.png";
+
+const HERO_SHOOT_FRAME_5 = new Image();
+HERO_SHOOT_FRAME_5.src = "./assets/soldier/shoot/Gun_Shot_005.png";
+
+const HERO_DIE_FRAME_0 = new Image();
+HERO_DIE_FRAME_0.src = "./assets/soldier/death/death_0000_Man.png";
+
+const HERO_DIE_FRAME_1 = new Image();
+HERO_DIE_FRAME_1.src = "./assets/soldier/death/death_0001_Man.png";
+
+const HERO_DIE_FRAME_2 = new Image();
+HERO_DIE_FRAME_2.src = "./assets/soldier/death/death_0002_Man.png";
+
+const HERO_DIE_FRAME_3 = new Image();
+HERO_DIE_FRAME_3.src = "./assets/soldier/death/death_0003_Man.png";
+
+const HERO_DIE_FRAME_4 = new Image();
+HERO_DIE_FRAME_4.src = "./assets/soldier/death/death_0004_Man.png";
+
+const HERO_DIE_FRAME_5 = new Image();
+HERO_DIE_FRAME_5.src = "./assets/soldier/death/death_0005_Man.png";
+
 class Menu {
 
     constructor(ctx) {
